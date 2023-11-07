920cfa2fc4968a62b26d79f91a0b086f4e55b031
renankanu <renankanu@gmail.com>
2023-11-07 09:23:20 -0300

    feat: change database

--- prisma/migrations/20231106140737_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `createdAt` on the `User` table. The data in that column could be lost. The data in that column will be cast from `DateTime(0)` to `DateTime`.
-
-*/
--- AlterTable
-ALTER TABLE `User` MODIFY `createdAt` DATETIME NOT NULL DEFAULT NOW();
